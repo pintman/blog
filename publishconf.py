ded208c50f6d4995b0d0cd86c866915b8848e160
Marco Bakera <marco@bakera.de>
2018-06-16 12:08:56 +0200

    changed site url for publishing.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.bakera.de/wp'
+#SITEURL = 'https://www.bakera.de/wp'
+SITEURL = 'https://pintman.gitlab.io/blog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
