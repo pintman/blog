f95f91281f4dec68e19f377b1c5d22208f2dc34b
Marco Bakera <bakera@tbs1.de>
2018-06-18 21:15:48 +0200

    fixed wrong link.

--- yt_add_video.sh
+++ yt_add_video.sh
@@ -7,13 +7,14 @@ VIDID=${1?"need video id"}
 URL=https://img.youtube.com/vi/$VIDID/hqdefault.jpg
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
-IMGPATH="content/images/$YEAR/$MONTH"
+IMGPATH1="images/$YEAR/$MONTH"
+IMGPATH2="content/$IMGPATH1"
 
 
-mkdir -p $IMGPATH
-wget -O $IMGPATH/$VIDID.jpg $URL
+mkdir -p $IMGPATH2
+wget -O $IMGPATH2/$VIDID.jpg $URL
 
 echo "Add to article:"
-echo ".. image:: $IMGPATH/$VIDID.jpg"
+echo ".. image:: $IMGPATH1/$VIDID.jpg"
 echo "   :alt: Youtube-Video"
 echo "   :target: https://www.youtube-nocookie.com/embed/${VIDID}?rel=0"
