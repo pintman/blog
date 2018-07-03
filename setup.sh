bc1b41e02b97d8f3310e5b68368f72b1ff973390
Marco Bakera <marco@bakera.de>
2018-07-03 19:25:45 +0200

    Revert "fixed version"
    This reverts commit 65cc85738a481fb4fd0ea215d14abb4cde9eed04.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-VERSION=3.7.1
-
 # install python packages
 # beautifulsoup is needed for plugin tipue search
-pip3 install pelican==$VERSION beautifulsoup4 markdown
+pip3 install pelican beautifulsoup4 markdown
 
 # install plugins repo
 test -d pelican-plugins || git clone https://github.com/getpelican/pelican-plugins
