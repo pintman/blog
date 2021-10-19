bc50e15b9e2556de29587add801ed7a2f06629b7
Marco Bakera <marco@bakera.de>
2021-10-19 23:32:40 +0200

    use poetry for dep mngmt

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-VERSION=4.5.3
+#VERSION=4.5.3
 # known stable release
 #PELICAN_THEMES_RELEASE=12d89d4
 #PELICAN_PLUGINS_RELEASE=d79a52ab820e0a7ea4bbf882a90b3086b367d72b
 
 # install python packages
 # beautifulsoup is needed for plugin tipue search
-pip3 install pelican==$VERSION beautifulsoup4 markdown
+#pip3 install pelican==$VERSION beautifulsoup4 markdown
+poetry install
 
 # install plugins repo
 test -d pelican-plugins || git clone https://github.com/getpelican/pelican-plugins
