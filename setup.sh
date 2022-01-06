79902794274a212e761dbd4f38387230fc877e45
Marco Bakera <marco@bakera.de>
2022-01-06 20:41:47 +0100

    rm old comments

--- setup.sh
+++ setup.sh
@@ -3,14 +3,7 @@
 # exit on error
 set -e
 
-#VERSION=4.5.3
-# known stable release
-#PELICAN_THEMES_RELEASE=12d89d4
-#PELICAN_PLUGINS_RELEASE=d79a52ab820e0a7ea4bbf882a90b3086b367d72b
-
 # install python packages
-# beautifulsoup is needed for plugin tipue search
-#pip3 install pelican==$VERSION beautifulsoup4 markdown
 poetry install
 
 # install plugins repo
