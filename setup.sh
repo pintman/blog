25e87eb634c787756fec4f7bedc22f4d0745d3fc
Marco Bakera <marco@bakera.de>
2022-01-07 07:42:04 +0100

    pinned versions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# git hashes as of 2022-01-07
+PLUGINS_VERSION=483215d2e1998684b7f413b6b1c8e26058fd2e6b
+THEMES_VERSION=565cb3fb8c88f1089502dc82a70fda5a7c7b322e
+
+
 # exit on error
 set -e
 
@@ -8,6 +13,8 @@ poetry install
 
 # install plugins repo
 test -d pelican-plugins || git clone https://github.com/getpelican/pelican-plugins
+git checkout -C pelican-plugins $PLUGINS_VERSION
+
 
 # hotfix for old plugin tipue search
 pushd pelican-plugins/tipue_search/
@@ -16,4 +23,4 @@ popd
 
 # install theme repo
 test -d pelican-themes || git clone https://github.com/getpelican/pelican-themes
-
+git checkout -C pelican-themes $THEMES_VERSION
