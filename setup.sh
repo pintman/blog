206e467e7688e03ac0d66495128548e5ef5af702
Marco Bakera <marco@bakera.de>
2022-01-07 07:50:21 +0100

    fix setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 PLUGINS_VERSION=483215d2e1998684b7f413b6b1c8e26058fd2e6b
 THEMES_VERSION=565cb3fb8c88f1089502dc82a70fda5a7c7b322e
 
-
 # exit on error
 set -e
 
@@ -13,14 +12,13 @@ poetry install
 
 # install plugins repo
 test -d pelican-plugins || git clone https://github.com/getpelican/pelican-plugins
-git checkout -C pelican-plugins $PLUGINS_VERSION
+git  -C pelican-plugins checkout $PLUGINS_VERSION
 
-
-# hotfix for old plugin tipue search
+echo "hotfix for old plugin tipue search"
 pushd pelican-plugins/tipue_search/
 cp -v pelican/plugins/tipue_search/* .
 popd
 
 # install theme repo
 test -d pelican-themes || git clone https://github.com/getpelican/pelican-themes
-git checkout -C pelican-themes $THEMES_VERSION
+git  -C pelican-themes checkout $THEMES_VERSION
